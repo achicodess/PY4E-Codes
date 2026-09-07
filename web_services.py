c1b36afd0ca92dbf4fd603f77204d968fb68a3d7
achicodess <achintyaa.m001@gmail.com>
2026-09-07 19:10:52 +0530

    Improved code: JSON and Conclusion

--- web_services.py
+++ web_services.py
@@ -16,5 +16,5 @@ elif opt==2:
 elif opt==3:
 	print("To conclude, Python actually let's you connect to the web and other programming languages using it's own set of libraries allowing for a multi-language application and integration.\n")
 else:
-	print("Program terminated.")
+	print("Program terminated.")   
 
